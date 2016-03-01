7bb593a90b3e105edd42e274c2459ea4721f13ca
basicdays <info@basicdays.com>
2016-03-01 21:43:02 +0000

    Fix get-dotfiles submodules

--- get-dotfiles.sh
+++ get-dotfiles.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-git clone --recursive "git://github.com/andsens/homeshick.git" "$HOME/.homesick/repos/homeshick"
+git clone "git://github.com/andsens/homeshick.git" "$HOME/.homesick/repos/homeshick"
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 homeshick clone basicdays/dotfiles
 homeshick cd dotfiles
-git remote set-url "git@github.com:basicdays/dotfiles.git"
+git remote set-url origin "git@github.com:basicdays/dotfiles.git"
 git submodule init
+git submodule update
 homeshick link dotfiles
