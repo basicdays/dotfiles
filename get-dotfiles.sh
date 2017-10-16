76ebac933d9e2d425d8fa2aa8bbaf8e954b2ba33
basicdays <info@basicdays.com>
2017-10-16 15:33:26 -0500

    Update files...

--- get-dotfiles.sh
+++ get-dotfiles.sh
@@ -4,5 +4,4 @@ git clone "git://github.com/andsens/homeshick.git" "$HOME/.homesick/repos/homesh
 homeshick clone basicdays/dotfiles
 homeshick cd dotfiles
 git remote set-url origin "git@github.com:basicdays/dotfiles.git"
-git submodule update --init --recursive
 homeshick link dotfiles
