41b344bbd79f90c739071948c384f1cc454bf216
basicdays <info@basicdays.com>
2020-10-27 15:46:19 -0500

    Update get script; update vscode config; add git prune script

--- get-dotfiles.sh
+++ get-dotfiles.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env sh
-set -e
+set -eu
+
 
 git clone "git://github.com/andsens/homeshick.git" "$HOME/.homesick/repos/homeshick"
+# shellcheck disable=1090
 . "$HOME/.homesick/repos/homeshick/homeshick.sh"
-homeshick clone basicdays/dotfiles
-homeshick cd dotfiles
-git remote set-url origin "git@github.com:basicdays/dotfiles.git"
+homeshick clone git@github.com:basicdays/dotfiles.git
+homeshick clone git@github.com:basicdays/dotfiles-macos.git
+homeshick clone git@github.com:basicdays/dotfiles-ubuntu.git
 homeshick link dotfiles
+
+case "$(uname -s)" in
+    Linux*)
+        homeshick link dotfiles-ubuntu
+        ;;
+    Darwin*)
+        homeshick link dotfiles-macos
+        ln -s "../../../../.config/Code/User/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+        ;;
+esac
